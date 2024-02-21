0cfbba802fd1fc0f891377e107899e43e494d227
plbonnier <pl_bonnier@yahoo.fr>
2024-02-21 21:55:46 +0100

    mise en place des ventes futurs dans accueil

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -14,7 +14,7 @@ class HomeController extends AbstractController
     public function index(VenteRepository $venteRepository, LotRepository $lotRepository): Response
     {
         $ventesPasses = $venteRepository->findBy(['passe' => true], ['dateVente' => 'DESC'], 2);
-        $ventesFutur = $venteRepository->findBy(['futur' => true], ['dateVente' => 'ASC'], 1);
+        $ventesFutur = $venteRepository->findBy(['futur' => true], ['dateVente' => 'ASC'], 2);
         $lotsFuturs = [];
         $selectedLotsFutur = [];
         $lotsPasses = [];
