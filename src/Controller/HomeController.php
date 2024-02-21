fb6bb7d7fd29980e498ca2ea70f803ed67775a9a
plbonnier <pl_bonnier@yahoo.fr>
2024-02-21 12:26:27 +0100

    simplification card dans page vente passe et creation de la method dans le controller pour les ventes passe, creation des card dans la vue en mobile et desktop

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Repository\LotRepository;
+use App\Repository\VenteRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,8 +11,25 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(VenteRepository $venteRepository, LotRepository $lotRepository): Response
     {
-        return $this->render('home/index.html.twig');
+        $ventesPasses = $venteRepository->findBy(['passe' => true], ['dateVente' => 'DESC'], 2);
+        $lots = [];
+        $selectedLotsPasses = [];
+
+        foreach ($ventesPasses as $vente) {
+            // Récupérer tous les lots pour la vente courante
+            $lots = $lotRepository->findByVente($vente->getId());
+
+            // Mélanger les lots aléatoirement
+            shuffle($lots);
+
+            // Sélectionner un nombre limité de lots, ici 3
+            $selectedLotsPasses[$vente->getId()] = array_slice($lots, 0, 2);
+        }
+        return $this->render('home/index.html.twig', [
+            'ventesPasses' => $ventesPasses,
+            'selectedLotsPasses' => $selectedLotsPasses
+            ]);
     }
 }
